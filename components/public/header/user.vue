4392159549c60c5669a7e474f3051f472c5f7397
王童孟 <tongmengwang@163.com>
2019-07-05 15:49:44 +0800

    1.2 首页Header-头部引导导航设计
    - 调整 header 布局分配 5 5 14
    - 导航bar

--- components/public/header/user.vue
+++ components/public/header/user.vue
@@ -2,6 +2,7 @@
   <div class="m-user">
     <template v-if="user">
       欢迎您，<span class="username">{{ user }}</span>
+      <nuxt-link to="/exit">退出</nuxt-link>
     </template>
     <template v-else>
       <nuxt-link to="login" class="login">立即登录</nuxt-link>
@@ -20,6 +21,6 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 </style>
