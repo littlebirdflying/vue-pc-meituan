c95f09e6ef72a3a8559a13e478bfd43348024eeb
王童孟 <tongmengwang@163.com>
2019-07-05 16:02:52 +0800

    1.3 首页Header-修改header结构
    header改为上下结构，由topbar和searchbar组成。

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,31 +1,24 @@
 <template>
-  <el-row :gutter="0" class="m-header">
-    <el-col :span="5">
-      <geo />
-    </el-col>
-    <el-col :span="5">
-      <user />
-    </el-col>
-    <el-col :span="14">
-      <navbar />
-    </el-col>
-</el-row>
+  <div class="m-header">
+    <el-row>
+      <el-col>
+        <top-bar />
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-  import Geo from './geo.vue'
-  import User from './user.vue'
-  import Navbar from './nav.vue'
+  import topBar from './topbar.vue'
+  import searchBar from './searchbar.vue'
   export default {
     components: {
-      Geo,
-      User,
-      Navbar
-    }
+      topBar,
+      searchBar
+    },
   }
 </script>
 
 <style lang="scss">
- @import "@/assets/css/public/layout.scss";
- @import "@/assets/css/public/header/index.scss";
+@import "@/assets/css/public/header/index.scss";
 </style>
